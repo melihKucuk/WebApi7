3ca0c3aecbfbbd12d578d535a6ab643f3d7a62ff
Melih Küçük <melihkucuk.mdbf20@iste.edu.tr>
2023-06-13 16:38:49 +0300

    Proje dosyası ekle.

---
+++ WebApi7/Controller/StajController.cs
@@ -0,0 +1,122 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace WebApi7.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StajController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public StajController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("MyDatabase");
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            using var con = new SqlConnection(_connectionString);
+            using var da = new SqlDataAdapter("Select * FROM staj1", con);
+            var dt = new DataTable();
+            da.Fill(dt);
+            return dt.Rows.Count > 0 ? JsonConvert.SerializeObject(dt) : "Veritabanında hicbir kayit bulunamadi";
+        }
+
+        [HttpGet("{id}")]
+        [Route("api/staj/{id}")]
+        public string Get(int id)
+        {
+            using var con = new SqlConnection(_connectionString);
+            using var da = new SqlDataAdapter("Select * FROM staj1 WHERE id = @id", con);
+            da.SelectCommand.Parameters.AddWithValue("@id", id);
+            var dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                return JsonConvert.SerializeObject(dt);
+            }
+            else
+            {
+                return "Veritabanında hiçbir kayıt bulunamadi";
+            }
+
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public string LastName { get; set; }
+        }
+
+        [HttpPost]
+        [Route("api/staj")]
+        public string Post([FromBody] Person person)
+        {
+            using var con = new SqlConnection(_connectionString);
+            using var cmd = new SqlCommand("Insert into staj1(Name, LastName) VALUES(@Name, @LastName)", con);
+            cmd.Parameters.AddWithValue("@Name", person.Name);
+            cmd.Parameters.AddWithValue("@LastName", person.LastName);
+            con.Open();
+            var i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i == 1)
+            {
+                return $"{person.Name} {person.LastName} Kayıtlara Eklendi";
+            }
+            else
+            {
+                return "Kayıt Eklenemedi Tekrar Dene";
+            }
+
+        }
+
+        [HttpPut("{id}")]
+        [Route("api/staj/{id}")]
+        public string Put(int id, [FromBody] Person person)
+        {
+            using var con = new SqlConnection(_connectionString);
+            using var cmd = new SqlCommand("UPDATE staj1 SET Name = @Name, LastName = @LastName WHERE id = @id", con);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@Name", person.Name);
+            cmd.Parameters.AddWithValue("@LastName", person.LastName);
+            con.Open();
+            var i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i == 1)
+            {
+                return $"{person.Name} {person.LastName} Adlı kişinin Kaydı Güncellendi";
+            }
+            else
+            {
+                return "Kayıt Güncellenemedi Tekrar Dene";
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        [Route("api/staj/{id}")]
+        public string Delete(int id)
+        {
+            using var con = new SqlConnection(_connectionString);
+            using var cmd = new SqlCommand("DELETE FROM staj1 WHERE id = @id", con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            var i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i == 1)
+            {
+                return "Kayıt Başarıyla Silindi";
+            }
+            else
+            {
+                return "Kayıt Silinemedi Tekrar Dene";
+            }
+
+        }
+    }
+}
---
+++ WebApi7/Startup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApi7
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Diðer baðýmlýlýklarýn eklenmesi ve yapýlandýrmalarýn yapýlmasý
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // ...
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "StajRoute",
+                    pattern: "api/staj/{id}",
+                    defaults: new { controller = "Staj", action = "Get" }
+                );
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
